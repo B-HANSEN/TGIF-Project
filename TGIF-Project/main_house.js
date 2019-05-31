642fb75ef8a021f9c2a5b1c8ac5820632373d0c6
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2019-05-31 16:40:33 +0200

    added email with logo, added photo to home page, provided links to senators' homepages

--- TGIF-Project/main_house.js
+++ TGIF-Project/main_house.js
@@ -8,33 +8,40 @@ function createTable(arrayMembers) {
         var lastName = arrayMembers[i].last_name; /// into members, select name IDs
         var firstName = arrayMembers[i].first_name;
         var middleName = arrayMembers[i].middle_name;
-
         var party = arrayMembers[i].party;
         var state = arrayMembers[i].state;
         var seniority = arrayMembers[i].seniority;
         var statePercentages = arrayMembers[i].votes_with_party_pct;
-
-        var td1 = document.createElement("td"); /// create data cell for last_name              
-        var td2 = document.createElement("td"); /// create data cell for first_name
-        var td3 = document.createElement("td"); /// create data cell for middle_name
-        var td4 = document.createElement("td"); /// create data cell for middle_name
-        var td5 = document.createElement("td"); /// create data cell for middle_name
-
+        var fullName;
 
         if (middleName == null) {
-            var fullName = `${lastName}, ${firstName}`;
+            fullName = `${lastName} ${firstName}`;
         } else {
-            var fullName = `${lastName}, ${firstName}, ${middleName}`;
+            fullName = `${lastName} ${firstName} ${middleName}`;
+        }
+        var td_fullName = document.createElement("td"); /// create data cell              
+        var td_party = document.createElement("td");
+        var td_state = document.createElement("td");
+        var td_seniority = document.createElement("td");
+        var td_statePercentages = document.createElement("td");
+
+        if (members[i].url != "") {
+            var membersURL = document.createElement("a");
+            membersURL.setAttribute("href", members[i].url);
+            membersURL.innerHTML = fullName;
+            membersURL.setAttribute("target", "_blank");
+            td_fullName.append(membersURL);
+        } else {
+            td_fullName.innerHTML = fullName
         }
 
         /// access array and define table cells
-        td1.innerHTML = fullName; /// set data cell td1 with item 0 in array results, property last_name
-        td2.innerHTML = party;
-        td3.innerHTML = state;
-        td4.innerHTML = seniority;
-        td5.innerHTML = statePercentages;
+        td_party.innerHTML = party;
+        td_state.innerHTML = state;
+        td_seniority.innerHTML = seniority;
+        td_statePercentages.innerHTML = statePercentages + " %";
 
-        tr.append(td1, td2, td3, td4, td5); /// append cells to rows
+        tr.append(td_fullName, td_party, td_state, td_seniority, td_statePercentages); /// append cells to rows
         tbody.append(tr); /// append rows to body
 
     }
