03d8e39ba1bf34102b0761d1dbaf9eb81da0d82a
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2019-06-06 21:51:27 +0200

    last status with revised Asc&Desc function that show correct results, pending duplicate consideration

--- TGIF-Project/styles/statistics.js
+++ TGIF-Project/styles/statistics.js
@@ -11,6 +11,7 @@ let statistics = {
     "Independents": {
         NoOfReps: 0,
         votedWithParty: 0,
+
     },
     "Total": {
         NoOfReps: data.results[0].num_results,
@@ -22,7 +23,12 @@ let statistics = {
 // update statistics object with the number of members in each party,
 // ... e.g. for the key "Number of Democrats" replace the default value of zero with the length of the list of Democrat objects.
 
-var arrayOfMembers = data.results[0].members; // declare variable and access in js.file that contains all members
+arrayOfMembers = data.results[0].members;
+
+var totalVotesPerc = 0;
+for (i = 0; i < arrayOfMembers.length; i++) {
+    totalVotesPerc += arrayOfMembers[i].votes_with_party_pct
+}
 
 // members by party
 statistics.Democrats.NoOfReps = numberOfMembers(arrayOfMembers, "D")
@@ -34,7 +40,7 @@ function numberOfMembers(anyArray, letter) {
     for (var i = 0; i < anyArray.length; i++) {
         var membersParty = anyArray[i].party;
         if (membersParty == letter) {
-            numberOfMembersByParty++; // creating 3 different groups
+            numberOfMembersByParty++;
         }
     }
     return numberOfMembersByParty;
@@ -48,7 +54,7 @@ if (sumVotes(arrayOfMembers, "I") !== 0) {
 } else {
     statistics.Independents.votedWithParty = 0;
 }
-statistics.Total.votedWithParty = ((Number(statistics.Democrats.votedWithParty) + Number(statistics.Republicans.votedWithParty) + Number(statistics.Independents.votedWithParty)) / 3).toFixed(2);
+statistics.Total.votedWithParty = (Number(totalVotesPerc / arrayOfMembers.length)).toFixed(2);
 
 function sumVotes(anyArray, letter) {
     var allVotes = 0;
@@ -84,30 +90,56 @@ var sorterDesc = [];
 
 // ascending all pct over the complete members array
 function mainArrayAsc(anyArray, criterion) {
-    for (var i = 0; i < anyArray.length; i++) {
-        sorterAsc.push(anyArray[i])
-    };
-    sorterAsc.sort(function (a, b) {
-        return b[criterion] - a[criterion];
-    });
-    sorterAsc = sorterAsc.slice(0, Math.round((anyArray.length * 0.1)));
-};
+    for (var m = 0; m < anyArray.length; m++) { // urspruenglich fill, sort, cut
+        sorterAsc.push(anyArray[m]);
+        sorterAsc.sort(function (a, b) {
+            return a[criterion] - b[criterion];
+        });
+        sorterAsc = sorterAsc.slice(0, Math.round((anyArray.length * 0.1))); of (anyArray)
+
+
+        // if (anyArray[i].party == letter) {
+        //     allVotes += anyArray[i].votes_with_party_pct;
+        // }
+
+        // if (m <= (anyArray.length * 0.1)) {
+        //     sorterAsc.push(anyArray[m])
+        // } else if (anyArray[m] == anyArray[m - 1]) {
+        //     sorterAsc.push(anyArray[m])
+        // } else {
+        //     break
+        // }
+        // sorterAsc.sort(function (a, b) {
+        //     return a[criterion] - b[criterion];
+        // });
+    }
+}
 if (document.URL.includes("attendance")) {
     mainArrayAsc(arrayOfMembers, "missed_votes_pct")
 } else {
     mainArrayAsc(arrayOfMembers, "votes_with_party_pct")
 }
-
 // descending all pct over the complete members array
 function mainArrayDesc(anyArray, criterion) {
-    for (var i = 0; i < anyArray.length; i++) {
-        sorterDesc.push(anyArray[i]);
-    };
-    sorterDesc.sort(function (a, b) {
-        return a[criterion] - b[criterion];
-    });
-    sorterDesc = sorterDesc.slice(0, Math.round((anyArray.length * 0.1)));
-};
+    for (var m = 0; m < anyArray.length; m++) {
+        sorterDesc.push(anyArray[m]);
+        sorterDesc.sort(function (a, b) {
+            return b[criterion] - a[criterion];
+        });
+        sorterDesc = sorterDesc.slice(0, Math.round((anyArray.length * 0.1)));
+
+        // if (m <= (anyArray.length * 0.1)) {
+        //     sorterDesc.push(anyArray[m])
+        // } else if (anyArray[m] == anyArray[m - 1]) {
+        //     sorterDesc.push(anyArray[m])
+        // } else {
+        //     break
+        // }
+        // sorterDesc.sort(function (a, b) {
+        //     return b[criterion] - a[criterion];
+        // });
+    }
+}
 if (document.URL.includes("attendance")) {
     mainArrayDesc(arrayOfMembers, "missed_votes_pct")
 } else {
@@ -134,25 +166,25 @@ function forHtmlTable1(anyArray, tbodyId) {
 
 function forHtmlTable2(anyArray, tbodyId) {
     var tbody = document.getElementById(tbodyId);
-    for (var k = 0; k < anyArray.length; k++) {
+    for (var n = 0; n < anyArray.length; n++) {
         var row = tbody.insertRow(-1);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
-        if (anyArray[k].middle_name == null) {
-            anyArray[k].middle_name = "";
+        if (anyArray[n].middle_name == null) {
+            anyArray[n].middle_name = "";
         }
-        cell1.innerHTML = `${anyArray[k].first_name} ${anyArray[k].middle_name} ${anyArray[k].last_name}`;
-        cell2.innerHTML = anyArray[k].total_votes;
-        cell3.innerHTML = anyArray[k].votes_with_party_pct;
+        cell1.innerHTML = `${anyArray[n].first_name} ${anyArray[n].middle_name} ${anyArray[n].last_name}`;
+        cell2.innerHTML = anyArray[n].total_votes;
+        cell3.innerHTML = anyArray[n].votes_with_party_pct;
         // cell3.innerHTML = anyArray[k][crt2];
     }
 }
 if (document.URL.includes("attendance")) {
     // forHtmlTable1((sorterAsc, "least"), missed_votes);
-    forHtmlTable1(sorterAsc, "least");
-    forHtmlTable1(sorterDesc, "top");
+    forHtmlTable1(sorterDesc, "least");
+    forHtmlTable1(sorterAsc, "top");
 } else {
-    forHtmlTable2(sorterDesc, "leastloyalty");
-    forHtmlTable2(sorterAsc, "toployalty");
+    forHtmlTable2(sorterAsc, "leastloyalty");
+    forHtmlTable2(sorterDesc, "toployalty");
 }
