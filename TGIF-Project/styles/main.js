d572f55c7229f81493906379268e415db5e50e51
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2019-06-12 09:58:51 +0200

    completed if-statements to consider all possible party combinations

--- TGIF-Project/styles/main.js
+++ TGIF-Project/styles/main.js
@@ -64,7 +64,7 @@ createTable(arrayOfMembers);
 
 // ********** working with checkboxes and filters **********
 
-// create array by party and push all info into it
+// create array by party; initialise
 var partyDems = [];
 var partyReps = [];
 var partyIndies = [];
@@ -84,14 +84,12 @@ function sorterFunction(anyArray) {
   }
 }
 
+// access Elements in html and declare variables
 dem = document.getElementById("Dems");
-console.log(partyDems);
 rep = document.getElementById("Reps");
-console.log(partyReps);
 ind = document.getElementById("Indies");
-console.log(partyIndies);
 
-// add EventListeners
+// add EventListeners to variables
 rep.addEventListener("click", function() {
   combineArrays();
 });
@@ -102,16 +100,30 @@ dem.addEventListener("click", function() {
   combineArrays();
 });
 
+// declare combined arrays with different party combinations
 var partyDemsInds = partyDems.concat(partyIndies);
+var partyDemsReps = partyDems.concat(partyReps);
+var partyIndReps = partyIndies.concat(partyReps);
 
-// function that checks if chechbox is checked
+// function that checks if chechbox is checked and invoke createTable function
 function combineArrays() {
   for (i = 0; i < arrayOfMembers.length; i++) {
-    if (dem.checked && ind.checked) {
-      console.log(ind.checked, dem.checked);
+    if (rep.checked && dem.checked && ind.checked) {
+      createTable(arrayOfMembers);
+    } else if (dem.checked && ind.checked) {
       createTable(partyDemsInds);
+    } else if (dem.checked && rep.checked) {
+      createTable(partyDemsReps);
+    } else if (ind.checked && rep.checked) {
+      createTable(partyIndReps);
+    } else if (ind.checked) {
+      createTable(partyIndies);
+    } else if (dem.checked) {
+      createTable(partyDems);
+    } else if (rep.checked) {
+      createTable(partyReps);
     } else {
-      createTable("");
+      createTable(arrayOfMembers);
     }
   }
 }
